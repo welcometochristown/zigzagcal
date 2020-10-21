5abb1b709de011dcf6e8fa6c0ff4682f246cd390
chris <welcometochristown@gmail.com>
2020-10-20 22:31:58 -0400

    new icons

--- client/src/components/NavMenu.js
+++ client/src/components/NavMenu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import logo from '../media/logo128.png';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -26,7 +27,7 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">ZigZagCal</NavbarBrand>
+            <NavbarBrand tag={Link} to="/"><img src={logo} class="logoimg"/> ZigZagCal </NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
